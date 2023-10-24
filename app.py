ed3742aac38b6548ad9ef01666f20f25deb0c409
wthoutjc <juancamilorr2012@hotmail.com>
2023-10-24 08:22:43 -0500

    db to temp

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ time_zone = config('TZ')
 os.environ['TZ'] = time_zone
 
 from src.create_app import create_app
+
+# Must for prod
+# Update the SQLite database file path to use /tmp
 app = create_app()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/instance/currency_db.db'
 
 # Lambda handler
 def lambda_handler(event, context):
@@ -16,5 +20,6 @@ def lambda_handler(event, context):
 
 if __name__ == '__main__':
     with app.app_context():
+        app.instance_path = '/tmp/instance'
         db.create_all()
     app.run(debug=True, host="0.0.0.0", port=5000)
